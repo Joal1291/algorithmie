5337ec8656fe57aa7e3cf790a4b6514f0cef9934
Joal1291 <jordan.albert@github.com>
2023-10-16 07:37:52 +0200

    ex21

--- javascript/exercice21.js
+++ javascript/exercice21.js
@@ -1,11 +1,19 @@
-// display whole number in a range of 1 to n int
+// display an array with all the whole number between 1 and n integer
 
-function displayWholeN(nbr){
-    let array = []
-    for(let i = 1; i <= nbr; i++){
-        if(i)
+function isWholeNumber(n){
+    if(n < 1 || n == 1 || n == 2 || n == 3){return true}
+    for(let i = 2; i <= n-1;i++){
+        if(n%i == 0){return false}
     }
+    return true
+}
+function displayWholeNumberInRange(nbr){
+    let array = [];
+    for(let i = 1; i < nbr ; i++){
+        if(isWholeNumber(i)){array.push(i); i++}
+        else{i++}
+    }
+    array.push(nbr)
     console.log(array)
 }
-
-displayWholeN(10)
+displayWholeNumberInRange(200)
