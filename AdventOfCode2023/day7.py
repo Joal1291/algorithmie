9a2493f0cdda62004583a62f49b8760214fe79b6
Joal1291 <jordan.albert@github.com>
2024-02-04 10:46:23 +0100

    day7

---
+++ AdventOfCode2023/day7.py
@@ -0,0 +1,13 @@
+import re
+import openfile
+
+from collections import Counter
+from functools import cmp_to_key
+
+exercice = openfile.readFile("day7.txt")
+exerciceTest = openfile.readFile("day7test.txt")
+
+### 250898830 ###
+#######################################################################
+#                                Part 1                               #
+#######################################################################
