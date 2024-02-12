142993d94d2d0f25e534733542f1f74d2c9468b6
Joal1291 <jordan.albert@github.com>
2024-02-12 22:00:54 +0100

    day 7

--- AdventOfCode2023/day7.py
+++ AdventOfCode2023/day7.py
@@ -1,4 +1,5 @@
 import re
+from collections import Counter # vas me permettre de compter les iteration d'un caractere dans une chaine de caractere
 import openfile
 
 from collections import Counter
@@ -11,3 +12,31 @@ exerciceTest = openfile.readFile("day7test.txt")
 #######################################################################
 #                                Part 1                               #
 #######################################################################
+
+####################################
+# Type du plus fort au plus faible #
+####################################
+# 5 carte du même type
+# 4 caret du même type
+# full house
+# 3 cartes du même type
+# 2 paire
+# 1 paire
+# carte forte
+
+# si deux main sont de même niveau comparer les carte de la premiere vers la 5.
+# La main qui l'emporte et celle avec la carte la plus forte
+# si par exemple la carte n°1 est de même type il faut donc comparer la deuxieme etc...
+# plus la main est forte plus le rank est élevé
+
+## LET'S GO.
+
+def puissance(array):
+    for lines in array:
+        line = lines.split()
+        main = Counter(line[0])
+         
+        print(line, main)
+
+
+puissance(exerciceTest) 
