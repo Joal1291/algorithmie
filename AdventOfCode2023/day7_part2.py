ee6c32fda3f0daf7c476f490c7ef13f40c7ff98c
Joal1291 <jordan.albert@github.com>
2024-02-14 10:34:06 +0100

    day7 over

--- AdventOfCode2023/day7_part2.py
+++ AdventOfCode2023/day7_part2.py
@@ -2,32 +2,23 @@ import re
 from collections import Counter # vas me permettre de compter les iteration d'un caractere dans une chaine de caractere
 import openfile
 
+from collections import Counter
+
 exercice = openfile.readFile("day7.txt")
 exerciceTest = openfile.readFile("day7test.txt")
 
 ### 250898830 ### 252127335 ###
 #######################################################################
-#                                Part 1                               #
+#                               Part 2                                #
 #######################################################################
 
-####################################
-# Type du plus fort au plus faible #
-####################################
-# 5 carte du même type
-# 4 caret du même type
-# full house
-# 3 cartes du même type
-# 2 paire
-# 1 paire
-# carte forte
+# le "J" est maintenant un joker il peut etre utiliser pour créer une main plus forte
+# exemple 'TJ55T' qui est une double paire  peut devenir 'TT55T' un full house sans en changer le caractere
+# pour pallier a ce fort avantage on nous dit que maintenant la valeur de J est inférieur a toute les autre carte
+# donc bien que celle ci ajoute un avantage lors du trie dans le tableau le J doit passer en dernier
+# exemple 'TJT55' contre 'T5J5T'. 'T5J5T'sera considérer comme plus forte car le 5 est au dessus du J donc elle aura un rank plus elever.
 
-# si deux main sont de même niveau comparer les carte de la premiere vers la 5.
-# La main qui l'emporte et celle avec la carte la plus forte
-# si par exemple la carte n°1 est de même type il faut donc comparer la deuxieme etc...
-# plus la main est forte plus le rank est élevé
-
-## LET'S GO.
-
+#LET'S GO
 # tableau des carte
 five = []
 four = []
@@ -36,9 +27,11 @@ three = []
 twoandtwo = []
 two = []
 high = []
+pasclasser = []
 
-# valeur
-topcard ={"2": 2, "3":3, "4":4, "5":5, "6":6, "7":7, "8":8, "9":9, "T":10, "J":11, "Q":12, "K":13, "A":14}
+# valeur a comparer
+topcard ={"2": 3, "3":4, "4":5, "5":6, "6":7, "7":8, "8":9, "9":10, "T":11, "J":2, "Q":12, "K":13, "A":14}
+
 
 def compareMain(main, mainInArray):
     count = 0
@@ -90,23 +83,36 @@ def findmain(array):
         line = lines.split()
         main = line[0]
         counter = Counter(main)
+        jack = main.count('J')
         # Counter permet dans ce cas de compter le nombre d'itération de chaque caractere de la main
         paire = counter.most_common(2)
         # most_common est un methode qui permet de récuperer les paire de clef valeur d'un résultat de Counter()
         # le chiffre préciser a l'intérieur des parenthése precise le nombre de paire clef valeur que l'on veut obtenir
-        valeur1 = paire[0][1]
+        clef1, valeur1 = paire[0][0], paire[0][1]
         if valeur1 == 5:
             storage(main, line, five)
             continue
         valeur2 = paire[1][1]
-        if valeur1 == 4:storage(main, line, four)
-        elif valeur1 == 3 and valeur2 == 2:storage(main, line, fullhouse)
-        elif valeur1 == 3:storage(main, line, three)
-        elif valeur1 == 2 and valeur2 == 2:storage(main, line, twoandtwo)
-        elif valeur1 == 2:storage(main, line, two)
-        else: storage(main, line, high)
+
+        valeur3 = valeur1 + jack
+
+        if clef1 == 'J' and valeur1 == 2 and valeur2 == 1:
+            storage(main, line, three)
+            continue
+        if clef1 == 'J':
+            valeur3 = valeur2 + jack 
+        
+        if valeur3 == 5:
+            storage(main, line, five)
+            continue
+
+        if valeur3  == 4:storage(main, line, four)
+        elif valeur3 == 3 and valeur2 == 2:storage(main, line, fullhouse)
+        elif valeur3 == 3: storage(main, line, three)
+        elif valeur3 == 2 and valeur2 == 2:storage(main, line, twoandtwo)
+        elif valeur3 == 2:storage(main, line, two)
+        elif valeur3 == 1: storage(main, line, high)
+        else:storage(main, line, pasclasser)
     print("Resultat:",calcul())
 
 findmain(exercice)
-
-print(five)
