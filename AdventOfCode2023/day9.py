c408ae9227e27f352642a0c44bb4e888a6e87bae
Joal1291 <jordan.albert@github.com>
2024-02-25 21:56:04 +0100

    15 d'écart sur le res attendu :(

---
+++ AdventOfCode2023/day9.py
@@ -0,0 +1,38 @@
+import re
+import openfile
+
+exercice = openfile.readFile("day9.txt")
+exerciceTest = openfile.readFile("day9test.txt")
+
+res = 0
+arrayBuffer = []
+
+def getResult(array):
+    value = 0
+    buf = []
+    for lines in array:
+        buf.append(int(lines[-1:][0]))
+    for i in range(1, len(buf)):
+            value = buf[i] + value
+    return value
+
+def allNextLine(line):
+    buffer = []
+    for i in range (1, len(line)):
+        buffer.append(int(line[i]) - int(line[i-1]))
+    arrayBuffer.insert(0,  buffer)
+    if sum(buffer) != 0:
+        allNextLine(buffer)
+
+def main(array):
+    result = 0
+    for lines in array:
+        line = lines.split()
+        arrayBuffer.append(line)
+        allNextLine(line)
+        result += getResult(arrayBuffer)
+
+        arrayBuffer.clear()
+    print("Result: ", result)
+
+main(exercice) 
