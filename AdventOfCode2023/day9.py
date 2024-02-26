4583a3b632f93159b2466d576672efe3567ecd02
Joal1291 <jordan.albert@github.com>
2024-02-26 12:47:41 +0100

    day9 part 1 over -a optimiser-

--- AdventOfCode2023/day9.py
+++ AdventOfCode2023/day9.py
@@ -4,13 +4,12 @@ import openfile
 exercice = openfile.readFile("day9.txt")
 exerciceTest = openfile.readFile("day9test.txt")
 
-res = 0
 arrayBuffer = []
 
 def getResult(array):
     value = 0
     for lines in array:
-        value += int(lines[-1:][0])
+        value = lines[-1:][0] + value
     return value
 
 def allNextLine(line):
@@ -20,15 +19,23 @@ def allNextLine(line):
     arrayBuffer.insert(0,  buffer)
     if sum(buffer) != 0:
         allNextLine(buffer)
+    else:
+        for i in buffer:
+            if i == 0:
+                continue
+            else:
+                allNextLine(buffer)
+                break
 
 def main(array):
     result = 0
     for lines in array:
-        line = lines.split(" ")
-        arrayBuffer.append(line)
-        allNextLine(line)
+        line = map(int, lines.split())
+        result_line = list(line)
+        arrayBuffer.append(result_line)
+        allNextLine(result_line)
         result += getResult(arrayBuffer)
-
+        
         arrayBuffer.clear()
     print("Result: ", result)
 
